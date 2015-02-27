d1070f3b36f9173fc5a40790f3bbd7a7acceb4da
eugen.covaci <eugen.covaci@ymens.com>
2015-02-27 11:24:41 +0200

    bbbb

--- just.for.testing/src/org/Main.java
+++ just.for.testing/src/org/Main.java
@@ -3,11 +3,7 @@ package org;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		//test 2 
-=======
-		//xxxxxxxx
->>>>>>> branch 'master' of https://github.com/ecovaci/org.just.for.test.git
+
 	}
 
 }
